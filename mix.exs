613b262b38ed016ba67d8c7b8a47665f37f0077e
John Wahba <john.a.wahba@gmail.com>
2017-08-05 16:01:44 -0700

    add enum player

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Vdemo2017.Mixfile do
   def application do
     [
       mod: {Vdemo2017.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :porcelain]
     ]
   end
 
@@ -40,7 +40,8 @@ defmodule Vdemo2017.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:porcelain, "~> 2.0"},
     ]
   end
 
