dc835d8026308221992d4e4be7e58982642a07be
John <jwahba@twitch.tv>
2017-09-07 07:26:49 -0700

    add sections

--- lib/vdemo2017_web/controllers/slide_controller.ex
+++ lib/vdemo2017_web/controllers/slide_controller.ex
@@ -4,4 +4,14 @@ defmodule Vdemo2017Web.SlideController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def start_stream(conn, _params) do
+    Vdemo2017.VideoStreamer.start()
+    render conn, "index.html"
+  end
+
+  def vote(conn, %{"vote" => vote}) do
+    Vdemo2017.Voter.vote(vote)
+    render conn, "index.html"
+  end
 end
